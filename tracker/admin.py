9926ee602abf66b750b9bb7147ff3e6f2da09576
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2019-11-01 09:34:13 +0000

    Improved admin page

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Homework
 
 # Register your models here.
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subject', 'due_date')
+    def save_model(self, request, obj, form, change):
+        obj.user_id = request.user.id
+        super().save_model(request, obj, form, change)
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Homework(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, editable=False)
+    subject = models.CharField(max_length=200)
+    task = models.TextField()
+    due_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.subject + str(self.due_date)
