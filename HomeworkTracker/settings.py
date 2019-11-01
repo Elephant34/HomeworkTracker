d002040139001313e9a5a199c0a5ee3a828c55c2
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2019-11-01 18:51:14 +0000

    Update settings.py
    Added proper secret key settings

--- HomeworkTracker/settings.py
+++ HomeworkTracker/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'two-hw9(-+%d3($-dcyz1)s+^nk$l*d)b#ezu@k_+6clul6)55'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
