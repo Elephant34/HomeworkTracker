9ffc309f240f907d90631d81f255408f57764a31
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2019-11-02 11:29:04 +0000

    Update settings.py
    Changed debug to false on release branch

--- HomeworkTracker/settings.py
+++ HomeworkTracker/settings.py
@@ -33,7 +33,7 @@ set_secret_key_env()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".pythonanywhere.com"]
 
